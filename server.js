59ee776afb3c8cb7cb9e815919aab50f7e1d927e
Eng-Abanoub <>
2021-08-10 00:58:33 +0200

    upload the project

---
+++ server.js
@@ -0,0 +1,32 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+// Start up an instance of app
+const app = express()
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors())
+//create function to send data to the end point server
+sendData=(req,res)=>{
+    projectData = req.body
+    console.log(projectData)
+    res.send()
+}
+
+app.post('/sendData',sendData)
+app.get('/getData',(req,res)=>{res.send(projectData)})
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+app.listen(3000,()=>{
+    console.log("server has been started")
+})
---
+++ website/app.js
@@ -0,0 +1,58 @@
+/* Global Variables */
+const API_KEY = "8cfb04d7d80f88aa77e0b6c0534c1c29";
+const generateBtn = document.getElementById('generate');
+const feelings = document.getElementById('feelings');
+const date = document.getElementById('date');
+const temp = document.getElementById('temp');
+const content = document.getElementById('content');
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'/'+ d.getDate()+'/'+ d.getFullYear();
+// create function to handle click
+let handleClick=()=> {
+    const zipcode = document.getElementById('zip').value;
+    console.log(zipcode);
+    console.log(feelings.value);
+    (zipcode.length>4)?getDate(zipcode,dataFlow):null;     
+}
+
+// add event listener to the generate button
+generateBtn.addEventListener('click',handleClick)
+
+// async function fetching api to get weather data
+async function getDate(zipcode,callback){
+    const url = `https:\\api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${API_KEY}&units=metric`
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    callback(data);
+}
+
+// crate async function dealing with the sever end point
+async function dataFlow(data){ 
+    await fetch('/sendData',{
+        method:'post',
+        credentials:"same-origin",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            date:newDate,
+            temp:data.main.temp,
+            feelings:feelings.value
+        }) 
+    })
+    const dataRes = await fetch("/getData")
+    const result = await dataRes.json()
+    console.log(result)
+    showData(result)
+}
+
+// creata a function to show up data 
+function showData(result){
+    date.innerText = `Date is : ${result.date}`
+    temp.innerText = `Temprature is : ${result.temp} C`
+    content.innerText = (result.feelings!='')?`Your feelings is :${result.feelings}`:'';
+}
+
